0bb4bdcc51a0b8e7c4365177ab213a3ac43bdf7f
pietrzakadrian <contact.pietrzakadrian@gmail.com>
2019-12-27 13:20:07 +0100

    redux-saga production task update

--- app/containers/WorkerPage/saga.js
+++ app/containers/WorkerPage/saga.js
@@ -2,6 +2,7 @@ import { call, put, select, takeLatest } from 'redux-saga/effects';
 import { api, request } from 'utils';
 import { GET_TASK, UPDATE_TASK } from './constants';
 import { makeSelectToken } from '../App/selectors';
+import { makeSelectTask } from './selectors';
 import {
   getTaskSuccessAction,
   getTaskErrorAction,
@@ -25,11 +26,16 @@ export function* getTask() {
 
 export function* updateTask() {
   const { accessToken } = yield select(makeSelectToken());
+  const { uuid } = yield select(makeSelectTask());
 
   try {
     const response = yield call(request, api.task, {
       method: 'PATCH',
-      headers: { Authorization: `Bearer ${accessToken}` },
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ uuid }),
     });
 
     yield put(updateTaskSuccessAction(response));
